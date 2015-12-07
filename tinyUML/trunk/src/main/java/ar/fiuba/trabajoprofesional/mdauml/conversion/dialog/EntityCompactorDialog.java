f483ef5c3206c58631a483ea83d15264f4267209
Fernando Romera Ferrio <fernandoromeraferrio@gmail.com>
2015-12-07 11:22:28 -0300

    Agrega merging de entidades en la conversion (falto subir archivos)

---
+++ tinyUML/trunk/src/main/java/ar/fiuba/trabajoprofesional/mdauml/conversion/dialog/EntityCompactorDialog.java
@@ -0,0 +1,158 @@
+package ar.fiuba.trabajoprofesional.mdauml.conversion.dialog;
+
+import ar.fiuba.trabajoprofesional.mdauml.ui.AppFrame;
+import ar.fiuba.trabajoprofesional.mdauml.util.Msg;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+
+public class EntityCompactorDialog extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JList allEntities;
+    private JTextField mergedEntity;
+    private JButton mergeButton;
+    private JButton add;
+    private JButton remove;
+    private JList mergingEntities;
+    private JLabel mergedLabel;
+    private JLabel mergingLabel;
+    private JLabel messageLabel;
+    private Map<String,String> mergingMap = new HashMap<>();
+    private Map<String,String> originalMap = new HashMap<>();
+    private List<String> entities  = new ArrayList<>();
+
+    public EntityCompactorDialog(Window parent, Map<String,String> mergingMap) {
+        super(parent,ModalityType.APPLICATION_MODAL);
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+        setTitle(Msg.get("conversion.dialog.entityCompactor.title"));
+        mergedLabel.setText(Msg.get("conversion.dialog.entityCompactor.mergedLabel"));
+        mergingLabel.setText(Msg.get("conversion.dialog.entityCompactor.mergingLabel"));
+        messageLabel.setText(Msg.get("conversion.dialog.entityCompactor.messageLabel"));
+        mergeButton.setText(Msg.get("conversion.dialog.entityCompactor.mergeButton"));
+        buttonOK.setText(Msg.get("stdcaption.ok"));
+        buttonCancel.setText(Msg.get("stdcaption.cancel"));
+
+        this.mergingMap=mergingMap;
+        this.originalMap.putAll(mergingMap);
+        initEntityList();
+        mergingEntities.setModel(new DefaultListModel());
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+// call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+// call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List<String> selected = allEntities.getSelectedValuesList();
+                DefaultListModel<String> model = ((DefaultListModel<String>) mergingEntities.getModel());
+                model.ensureCapacity(model.size()+selected.size());
+                for(int i = 0 , j=model.size(); i < selected.size() ; i++) {
+                    if(model.contains(selected.get(i)))
+                        continue;
+                    model.add(j++,selected.get(i));
+                }
+            }
+        });
+        remove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List<String> selectedEntities = mergingEntities.getSelectedValuesList();
+                DefaultListModel<String> model = ((DefaultListModel<String>) mergingEntities.getModel());
+                for(String entity : selectedEntities){
+                    model.removeElement(entity);
+                }
+
+            }
+        });
+        mergeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultListModel<String> model = ((DefaultListModel<String>) mergingEntities.getModel());
+                List<String> mergingElements = new ArrayList<String>();
+                for (int i = 0 ; i < model.size() ; i++)
+                    mergingElements.add(i,model.get(i));
+                String merged = mergedEntity.getText();
+
+                if(mergingElements.size()<2){
+                    JOptionPane.showMessageDialog(AppFrame.get(), Msg.get("conversion.dialog.entityCompactor.moreThan2"));
+                    return;
+                }
+                if(merged.isEmpty()) {
+                    JOptionPane.showMessageDialog(AppFrame.get(), Msg.get("conversion.dialog.entityCompactor.emptyField"));
+                    return;
+                }
+                doMerge(mergingElements,merged);
+                model.clear();
+                initEntityList();
+                mergedEntity.setText("");
+
+
+
+            }
+        });
+    }
+
+    private void doMerge(List<String> mergingElements, String merged) {
+        for(String mergingEntity : mergingElements)
+            for(String entity : mergingMap.keySet())
+                if(mergingMap.get(entity).equals(mergingEntity))
+                    mergingMap.put(entity,merged);
+
+    }
+
+    private void initEntityList() {
+        entities.clear();
+        entities.addAll(new HashSet<>(mergingMap.values()));
+        Collections.sort(entities);
+        DefaultListModel<String> model = new DefaultListModel<String>();
+        model.ensureCapacity(entities.size());
+        for(int i=0; i < entities.size(); i++)
+            model.add(i,entities.get(i));
+        allEntities.setModel(model);
+    }
+
+    private void onOK() {
+
+        dispose();
+    }
+
+    private void onCancel() {
+        mergingMap.clear();
+        mergingMap.putAll(originalMap);
+        dispose();
+    }
+
+
+
+
+}
--- tinyUML/trunk/src/main/java/ar/fiuba/trabajoprofesional/mdauml/conversion/impl/EntityCompactorImpl.java
+++ tinyUML/trunk/src/main/java/ar/fiuba/trabajoprofesional/mdauml/conversion/impl/EntityCompactorImpl.java
@@ -1,12 +1,41 @@
 package ar.fiuba.trabajoprofesional.mdauml.conversion.impl;
 
 import ar.fiuba.trabajoprofesional.mdauml.conversion.EntityCompactor;
+import ar.fiuba.trabajoprofesional.mdauml.conversion.dialog.EntityCompactorDialog;
+import ar.fiuba.trabajoprofesional.mdauml.conversion.dialog.MainEntityDialog;
 import ar.fiuba.trabajoprofesional.mdauml.exception.CompactorException;
+import ar.fiuba.trabajoprofesional.mdauml.exception.ConversionCanceledException;
 import ar.fiuba.trabajoprofesional.mdauml.model.UmlModel;
+import ar.fiuba.trabajoprofesional.mdauml.model.UmlUseCase;
+import ar.fiuba.trabajoprofesional.mdauml.ui.AppFrame;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class EntityCompactorImpl implements EntityCompactor {
+
+
     @Override
     public UmlModel compact(final UmlModel model) throws CompactorException {
-        return null; //TODO implement
+        Set<UmlUseCase> useCases = (Set<UmlUseCase>) model.getAll(UmlUseCase.class);
+        Map<String,String> entities = new HashMap<>();
+        for(UmlUseCase useCase: useCases){
+            for(String entity : useCase.getAllEntities())
+                entities.put(entity,entity);
+        }
+        openCompactorDialog(entities);
+        return updateModel(model,entities);
+    }
+
+    private UmlModel updateModel(UmlModel model, Map<String, String> entities) {
+        return null;
+    }
+
+    private void openCompactorDialog(Map<String, String> entities) {
+        EntityCompactorDialog dialog = new EntityCompactorDialog(AppFrame.get(),entities);
+        dialog.setLocationRelativeTo(AppFrame.get());
+        dialog.setVisible(true);
+
     }
 }
